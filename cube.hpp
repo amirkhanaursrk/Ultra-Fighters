3fd6d8103c86ff804c759ce785bcd7ce9a7f6258
Jeffrey A. Ryan <jeffaryan7@gmail.com>
2015-11-07 14:36:37 -0600

    Added Suzanne

---
+++ cube.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "game_object.hpp"
+
+class Cube: public GameObject {
+    public:
+        void setVPM(glm::mat4 VPM) override;
+        void setup() override;
+        void update(double step) override;
+        void render(float interp) override;
+    
+    private:
+        glm::mat4* MVP;
+        GLuint vao;
+        GLuint program;
+        int length;
+};
